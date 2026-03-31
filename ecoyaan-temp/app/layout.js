e21b404098d882320c3ac14426bacef656a5e623
Aishwar <aishwar.16218@gmail.com>
2026-03-31 18:03:37 +0530

    yes

--- ecoyaan-temp/app/layout.js
+++ ecoyaan-temp/app/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../styles/globals.css";
 import { AddressProvider } from "../context/AddressContext";
 import Header from "../components/Header";
 import StickyFooter from "../components/StickyFooter";
